3fd198dcfed95a3279f763e56f24b52472ce488c
David Ola <doola@ualberta.ca>
2026-08-15 20:39:01 -0600

    Testing sending file over net with C -> Finished client

--- src/config/dummy_client.c
+++ src/config/dummy_client.c
@@ -7,6 +7,9 @@
 #include <errno.h>
 #include <string.h>
 
+#include <sys/socket.h>
+#include <netdb.h>
+
 /*
     File functions:
     1. fopen() - Creates a FILE struct and returns a pointer, or null if it wasn't able to find
@@ -21,23 +24,65 @@
     6. ftell() - Returns the current file position
 */
 
+void* send_file_contents(int sock, FILE* fp) {
+    if (fp == NULL) {
+        printf("Error Number %d: %s\n", errno, strerror(errno));
+        return NULL;
+    }
+
+    long pos_addr = 0;
+
+    fseek(fp, 0, SEEK_END);
+    long max_addr = ftell(fp);
+    fseek(fp, 0, SEEK_SET); // setting the program counter to the beginning
+
+    printf("Max possible byte sent: %d\n", (int) max_addr);
+
+    char request[SEGMENT_LEN];
+    int incr = 0;
+    while (pos_addr < max_addr) {
+        fread(request, SEGMENT_LEN, sizeof(char), fp);
+        pos_addr += SEGMENT_LEN;
+
+        int bytes_sent = (int) send(sock, request, SEGMENT_LEN, 0);
+        printf("Increment %d.... %d bytes sent... \n", incr++, bytes_sent);
+    }
+
+    return 0;
+}
+
+typedef struct addrinfo addrinfo;
+
 int main() {
-    char* cwd = getcwd(NULL, 0);
-    printf("Working directory is: %s\n", cwd);
-    free(cwd);
+    /*
+      getaddrinfo to generate struct addrinfos that best match our server and our hints. Can
+      loop through until we get one that is ideal/exact.
+
+      socket creation then connect the socket to the addrinfo. connect() assigns it an ephemeral
+      (temporary port assigned by the OS since we don't need to care about the assigned port)
+
+      call send_file_contents to send contents to the server
+     */
 
     char* file_path = "../../src/config/in.txt"; // path relative to cmake
     FILE* fp = fopen(file_path, "rb");
 
-    if (fp == NULL) {
-        printf("Error Number %d: %s\n", errno, strerror(errno));
-        return -1;
-    }
+    int peer_sock;
 
-    char buffer[SEGMENT_LEN];
-    int program_counter = 1;
+    addrinfo hints;
+    memset(hints, 0 ,sizeof(hints));
 
-    fseek(fp, 0, SEEK_END);
-    long final_pos = ftell(fp);
-    printf("%d\n", (int) final_pos);
+    addrinfo *peer_addrinfo;
+
+    int res = getaddrinfo("8080", "0", &hints, &peer_addrinfo);
+    VERIFY_RSLT_RTRN(res, "addrinfo struct generation");
+
+    res = socket(AI_FAMILY, AI_SOCKTYPE, AI_PROTOCOL);
+    VERIFY_RSLT_RTRN(res, "socket creation");
+
+    res = connect(peer_sock, peer_addrinfo->ai_addr, peer_addrinfo->ai_addrlen);
+    VERIFY_RSLT_RTRN(res, "peering to server");
+
+    printf("Server connection has been established - Initiating steps to send contents to server!");
+    send_file_contents(peer_sock, fp);
 }
--- src/config/dummy_server.c
+++ src/config/dummy_server.c
@@ -1,6 +1,11 @@
 #include "../../include/utils.h"
 
 
+
+int recv_file_contents() {
+
+}
+
 int main() {
 
 }
